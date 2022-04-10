ec83d1c2518d269e043c89c80d1567f2e2c9fff7
Dennis Kupec <denniskupec@gmail.com>
2022-04-10 14:39:07 -0500

    Update force-disk-compatibility.sh

--- force-disk-compatibility.sh
+++ force-disk-compatibility.sh
@@ -7,11 +7,9 @@ for device in $(/usr/bin/ls /run/synostorage/disks); do
 
 	echo 'support' > $PREFIX/compatibility
 	echo 'support' > $PREFIX/force_compatibility
-  
-  # sourced from /var/lib/disk-compatibility/host_rule.db
+
+	# sourced from /var/lib/disk-compatibility/host_rule.db
 	echo '{"discard_from_web":false,"notification":false,"selectable":true,"alert":false,"ui_compatibility":"support"}' > $PREFIX/compatibility_action
 done
-
-/usr/syno/bin/syno_disk_db_update
 
 exit 0
